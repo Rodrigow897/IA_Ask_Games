69fa3f522a49f93fba7cbf88a207bdc9bb8f11ba
Rodrigo <rwesley2000@gmail.com>
2025-07-17 12:27:09 -0300

    starting logic

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const apiKeyInput = document.getElementById('apiKey');
 const gameSelect = document.getElementById('gameSelect');
 const questionInput = document.getElementById('questionInput');
+const askButton = document.getElementById('askButton');
+const aiResponse = document.getElementById('aiResponse');
+const form = document.getElementById('form');
 
-console.log(questionInput)
+const sendForm = (event) => {
+    event.preventDefault() //para nao atualizar e reccaregar a pagina ao clicar para enviar o form
+    const apiKey = apiKeyInput.value
+    const game = gameSelect.value
+    const question = questionInput.value
+
+    if (apiKey == '' || game == '' || question == '') {
+        alert('Por favor, preencha todos os campos')
+        return
+    }
+
+    askButton.disabled = true
+    askButton.textContent = "Perguntando..."
+    askButton.classList.add('loading')
+}
+form.addEventListener('submit', sendForm) //adiciona um escutador de evento para saber sempre que der um submit
