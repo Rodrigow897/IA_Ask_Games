54a6577dd90ae047a3a256714460dbf9ea312297
Rodrigo <rwesley2000@gmail.com>
2025-07-16 23:22:09 -0300

    starting script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const apiKeyInput = document.getElementById('apiKey');
+const gameSelect = document.getElementById('gameSelect');
+const questionInput = document.getElementById('questionInput');
+
+console.log(questionInput)
